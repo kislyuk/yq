16e1165c063218cdff923b80162ca4a00708959b
Andrey Kislyuk <kislyuk@gmail.com>
2017-01-14 22:04:03 -0800

    Shorten description in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license="Apache Software License",
     author="Andrey Kislyuk",
     author_email="kislyuk@gmail.com",
-    description="yq: Command-line YAML processor - jq wrapper for YAML documents",
+    description="Command-line YAML processor - jq wrapper for YAML documents",
     long_description=open("README.rst").read(),
     install_requires=[
         "setuptools",
