ba792a6231fa378704d177a524a3ca355688effe
Andrey Kislyuk <kislyuk@gmail.com>
2017-05-03 20:04:53 +0200

    Add noqa marker

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os, sys, unittest, tempfile, json, io
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from yq import main
+from yq import main # noqa
 
 USING_PYTHON2 = True if sys.version_info < (3, 0) else False
 
