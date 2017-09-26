7bc3d033e4b3797dbf79bc7d46a026a2f591a858
Andrey Kislyuk <kislyuk@gmail.com>
2017-09-25 22:54:00 -0700

    v2.3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ tests_require = ["coverage", "flake8", "wheel"]
 
 setup(
     name="yq",
-    version="2.3.1",
+    version="2.3.2",
     url="https://github.com/kislyuk/yq",
     license="Apache Software License",
     author="Andrey Kislyuk",
