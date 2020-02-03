d74511779baec8c1db60a7241b0683b740894289
Andrey Kislyuk <kislyuk@gmail.com>
2020-02-03 08:25:46 -0800

    Add RTD workaround

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-import guzzle_sphinx_theme
+import os
 
 project = "yq"
 copyright = "Andrey Kislyuk"
@@ -11,17 +11,23 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 source_suffix = [".rst", ".md"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 pygments_style = "sphinx"
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = "guzzle_sphinx_theme"
-html_theme_options = {
-    "project_nav_name": project,
-    "projectlink": "https://github.com/kislyuk/" + project,
-}
-html_sidebars = {
-    "**": [
-        "logo-text.html",
-        # "globaltoc.html",
-        "localtoc.html",
-        "searchbox.html"
-    ]
-}
+
+if "readthedocs.org" in os.getcwd().split("/"):
+    with open("index.rst", "w") as fh:
+        fh.write("Documentation for this project has moved to https://kislyuk.github.io/" + project)
+else:
+    import guzzle_sphinx_theme
+    html_theme_path = guzzle_sphinx_theme.html_theme_path()
+    html_theme = "guzzle_sphinx_theme"
+    html_theme_options = {
+        "project_nav_name": project,
+        "projectlink": "https://github.com/kislyuk/" + project,
+    }
+    html_sidebars = {
+        "**": [
+            "logo-text.html",
+            # "globaltoc.html",
+            "localtoc.html",
+            "searchbox.html"
+        ]
+    }
