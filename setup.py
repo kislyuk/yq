3043ec52ee7baa1f1fcb64b8084ba7f752204bf1
Andrey Kislyuk <andrey.kislyuk@color.com>
2024-04-13 10:02:55 -0700

    Remove Python 3.7 CI and metadata

--- setup.py
+++ setup.py
@@ -44,11 +44,11 @@ setup(
         "Operating System :: POSIX",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
