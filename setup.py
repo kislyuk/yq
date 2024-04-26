913a2ac6d82bf389abacc8f372536a836a3d3075
Andrey Kislyuk <andrey.kislyuk@color.com>
2024-04-26 10:47:56 -0700

    Constrain setuptools_scm version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     use_scm_version={
         "write_to": "yq/version.py",
     },
-    setup_requires=["setuptools_scm >= 3.4.3"],
+    setup_requires=["setuptools_scm >= 7, <8"],
     install_requires=[
         "PyYAML >= 5.3.1",
         "xmltodict >= 0.11.0",
